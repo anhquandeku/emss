69cab6a05e89495b1ff5787be125c0757d556558
Anh Quân <phananhquan48@gmail.com>
2022-04-27 08:32:31 +0700

    Update footer

--- app/View/__partial/footer.php
+++ app/View/__partial/footer.php
@@ -1,11 +1,11 @@
 <footer>
-    <div class="footer clearfix mb-0 text-muted">
+    <div class="footer clearfix mb-0 text-muted" style="padding-left:2%; padding-right:2%">
         <div class="float-start">
             <p>2021 &copy; Đại Học Sài Gòn</p>
         </div>
         <div class="float-end">
             <p>
-                Được tạo 
+                Được tạo
                 <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
                 bởi <a href="#">Vip Pro Team</a>
             </p>
