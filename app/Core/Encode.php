5de4c4fca477fe04a4969b75981d9e12a5195ba9
Anh Quân <phananhquan48@gmail.com>
2022-05-01 09:18:23 +0700

    Add Eculid

--- app/Core/Encode.php
+++ app/Core/Encode.php
@@ -1,11 +1,38 @@
 <?php
+namespace App\Core;
+
 class Encode
 {
-    public static function encode($data){
+    public static function Euclide($num, $a){
+        $n=$num;
+        $x2=1; $x1=0; $y2=0; $y1=1;
+        while($a>0){
+            $q=$n/$a;
+            $r=$n-$q*$a;
+            $x=$x2-$q*$x1;
+            $y=$y2-$q*$y1;
+            $n=$a;
+            $a=$r;
+            $x2=$x1;
+            $x1=$x;
+            $y2=$y1;
+            $y1=$y;            
+        }
+        return $num+$y2;
+    }
+
+    public static function encodeAES($data){
         
     }
 
-    public static function decryption($data){
+    public static function decryptionAES($data){
+        
+    }
+    public static function encodeRSA($data){
+        
+    }
+
+    public static function decryptionRSA($data){
         
     }
 }
